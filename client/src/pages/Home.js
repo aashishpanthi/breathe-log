4c438bd41bc774a797279f3ead2b1db132807722
Aashish Panthi <aashishpanthi11@gmail.com>
2023-05-27 18:29:11 +0545

    Add required pages

---
+++ client/src/pages/Home.js
@@ -0,0 +1,21 @@
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+
+const Home = () => {
+  return (
+    <>
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Hero */}
+
+      <Hero />
+
+      {/* Recommendation */}
+
+      {/* History */}
+    </>
+  );
+};
+
+export default Home;
